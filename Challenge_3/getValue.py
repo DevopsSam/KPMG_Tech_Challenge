222d8fb33076adeaec89961111a6d92a8a66101b
DevopsSam <samratjitmukherjee@gmail.com>
2021-05-21 23:21:13 +0530

    Add files via upload
    This folder contains a Python script with the solution for Challenge 3

---
+++ Challenge_3/getValue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+def get_value(value, key):
+    keys = key.split('/')
+    for key in keys:
+        value = value.pop(key)
+    return value
+
+object = {'x': {'y': {'z': 'a'}}}
+key = 'x/y/z'
+
+print ("Value is:", get_value(object, key))
